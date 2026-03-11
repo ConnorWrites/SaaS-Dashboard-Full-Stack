dc86175b3cadee339ae6bb41b7eb4063dd8818a0
Conrad Wilken <conradwilken@Conrads-MacBook-Pro.local>
2026-03-11 15:48:32 +0200

    Added location to ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { checkAuth } from "../services/api";
 
 export default function ProtectedRoute({ children }) {
   const [loading, setLoading] = useState(true);
   const [authorized, setAuthorized] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
+    setLoading(true);
     checkAuth()
       .then(() => setAuthorized(true))
       .catch(() => setAuthorized(false))
       .finally(() => setLoading(false));
-  }, []);
+  }, [location.pathname]);
 
   if (loading) return null; // or spinner
 
