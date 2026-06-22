a302db013acaab7025a17f49f259bfe8f57a2301
Conrad Wilken <conradwilken@Conrads-MacBook-Pro.local>
2026-06-22 16:02:06 +0200

    Added preliminary settings page

---
+++ client/src/pages/Settings.jsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Settings</h1>
+      <p>Here you can manage your settings.</p>
+    </div>
+  );
+}
